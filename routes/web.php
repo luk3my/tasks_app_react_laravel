bc313b07b521b395ea865dc2dfb7be151b1aa7fc
luk3my <luk3myoung@gmail.com>
2020-04-15 17:29:06 +1000

    react scaffolding

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Auth::routes();
+
+//Create a group of routes that are protected by the auth middleware - must be authenticated to view
+Route::group(['middleware' => ['auth']], function() {
+     Route::get('/', 'TasksIndexController@index');   
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
